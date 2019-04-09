eae2a53bc916a662b8a7ddbbdb53106405b75e2f
Fergoth <fergothew@gmail.com>
2019-04-09 10:12:41 +0300

    Fixed function name

--- pprint_json.py
+++ pprint_json.py
@@ -5,11 +5,10 @@ import sys
 def load_data(filepath):
     with open(filepath) as f:
         json_string = f.read()
-        json.loads(json_string)
         return json.loads(json_string)
 
 
-def pretty_print_json(json_data):
+def pretty_print_decoded_json(json_data):
     print(json.dumps(json_data, sort_keys=True, indent=4, ensure_ascii=False))
 
 
@@ -17,7 +16,7 @@ if __name__ == '__main__':
     try:
         path = sys.argv[1]
         data_from_file = load_data(path)
-        pretty_print_json(data_from_file)
+        pretty_print_decoded_json(data_from_file)
     except IndexError:
         print('Требуется путь к файлу как аргумент')
     except FileNotFoundError:
